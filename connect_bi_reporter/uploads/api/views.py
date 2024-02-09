50cee82828668f55095d0c7bac9494cc3e6b85df
Jonathan Rios <jonathanrios@live.com.ar>
2024-02-09 16:43:22 +0100

    LITE-29331 User can retry a failed upload

--- connect_bi_reporter/uploads/api/views.py
+++ connect_bi_reporter/uploads/api/views.py
@@ -1,12 +1,21 @@
+from logging import Logger
 from typing import List
 
 from fastapi import Depends, status
+from connect.client import ConnectClient
 from connect.eaas.core.decorators import router
-from connect.eaas.core.inject.synchronous import get_installation
+from connect.eaas.core.inject.common import get_call_context, get_logger
+from connect.eaas.core.inject.models import Context
+from connect.eaas.core.inject.synchronous import get_installation, get_installation_client
 from connect_extension_utils.db.models import get_db, VerboseBaseSession
 
+from connect_bi_reporter.scheduler import Scheduler
 from connect_bi_reporter.uploads.api.schemas import map_to_upload_schema, UploadSchema
-from connect_bi_reporter.uploads.services import get_upload_or_404, get_uploads_or_404
+from connect_bi_reporter.uploads.services import (
+    get_upload_or_404,
+    get_uploads_or_404,
+    retry_failed_upload,
+)
 
 
 class UploadsWebAppMixin:
@@ -42,3 +51,23 @@ class UploadsWebAppMixin:
             map_to_upload_schema(upload)
             for upload in get_uploads_or_404(db, installation, feed_id)
         ]
+
+    @router.post(
+        '/feeds/{feed_id}/uploads/{upload_id}/retry',
+        summary='Retry the require Upload which was previously failed',
+        response_model=UploadSchema,
+        status_code=status.HTTP_200_OK,
+    )
+    def get_retry_upload(
+        self,
+        feed_id: str,
+        upload_id: str,
+        db: VerboseBaseSession = Depends(get_db),
+        client: ConnectClient = Depends(get_installation_client),
+        context: Context = Depends(get_call_context),
+        logger: Logger = Depends(get_logger),
+        installation: dict = Depends(get_installation),
+    ):
+        scheduler = Scheduler(client, context, logger)
+        upload = retry_failed_upload(db, installation, feed_id, upload_id, scheduler)
+        return map_to_upload_schema(upload)
