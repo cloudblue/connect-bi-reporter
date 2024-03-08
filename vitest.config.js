4deb3a68a0b763cdfdd59243333d4f3632853a08
Arnau Giralt <50662025+arnaugiralt@users.noreply.github.com>
2024-03-08 13:50:52 +0100

    Update vitest config (#41)
    * Update vitest config
    - Add development mode flag to watch script
    - Create precommit script to run test, lint, format and build scripts sequentially
    - Update lint-format script to run sequentially
    - Update account_settings_page decorator label
    - Update dependencies

--- vitest.config.js
+++ vitest.config.js
@@ -5,22 +5,24 @@ import { mergeConfig, defineConfig, configDefaults } from 'vitest/config';
 
 import viteConfig from './vite.config';
 
-export default mergeConfig(
-  viteConfig,
-  defineConfig({
-    test: {
-      globals: true,
-      globalSetup: resolve(__dirname, 'ui/tests/config/vitest-global-setup.js'),
-      environment: 'jsdom',
-      exclude: [...configDefaults.exclude, 'e2e/*'],
-      root: fileURLToPath(new URL('./ui/', import.meta.url)),
-      reporters: ['json', 'default'],
-      outputFile: resolve(__dirname, 'ui/tests/report.json'),
-      coverage: {
-        enabled: false,
-        provider: 'v8',
-        reportsDirectory: resolve(__dirname, 'ui/tests/coverage'),
+export default defineConfig((configEnv) =>
+  mergeConfig(
+    viteConfig(configEnv),
+    defineConfig({
+      test: {
+        globals: true,
+        globalSetup: resolve(__dirname, 'ui/tests/config/vitest-global-setup.js'),
+        environment: 'jsdom',
+        exclude: [...configDefaults.exclude, 'e2e/*'],
+        root: fileURLToPath(new URL('./ui/', import.meta.url)),
+        reporters: ['json', 'default'],
+        outputFile: resolve(__dirname, 'ui/tests/report.json'),
+        coverage: {
+          enabled: false,
+          provider: 'v8',
+          reportsDirectory: resolve(__dirname, 'ui/tests/coverage'),
+        },
       },
-    },
-  }),
+    }),
+  ),
 );
