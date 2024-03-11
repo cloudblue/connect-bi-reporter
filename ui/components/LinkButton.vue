ab254011a46d6a5279c3f63c1b89fd305c100358
Arnau Giralt <arnau.giralt@cloudblue.com>
2024-03-11 12:07:54 +0100

    Update the LinkButton component - Added option to show an icon - Updated styles

--- ui/components/LinkButton.vue
+++ ui/components/LinkButton.vue
@@ -1,5 +1,11 @@
 <template>
-  <button class="link-button reset-button">
+  <button class="reset-button link-button">
+    <ui-icon
+      v-if="icon"
+      :iconName="icon"
+      size="16px"
+      :color="COLORS_DICT.NICE_BLUE"
+    />
     <slot>
       {{ text }}
     </slot>
@@ -7,8 +13,14 @@
 </template>
 
 <script setup>
+import { COLORS_DICT } from '~/constants/colors.js';
+
 defineProps({
   text: {
+    type: String,
+    default: '',
+  },
+  icon: {
     type: String,
     default: '',
   },
@@ -20,5 +32,12 @@ defineProps({
   text-decoration: underline;
   color: var(--color-nice-blue);
   cursor: pointer;
+  display: inline-flex;
+  align-items: center;
+}
+
+.link-button ui-icon {
+  margin-right: 4px;
+  display: inline-flex;
 }
 </style>
